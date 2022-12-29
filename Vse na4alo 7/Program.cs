8120bf20e1ab0e9cb49e9a9ea099c9ee0d697434
Irfan Yanbukhtin <woolfyko@gmail.com>
2022-12-29 21:31:57 +0400

    777

---
+++ Vse na4alo 7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Vse_na4alo_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Range a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Range b");
+            int b = int.Parse(Console.ReadLine());
+            int digitCount;
+            int count = 0;
+
+
+            for (int i = a; i <= b; i++)
+            {
+                digitCount = (int)Math.Log10(i);
+                int r = i / (int)Math.Pow(10, digitCount);
+                if (r == 7)
+                    count++;
+
+            }
+
+            int[] sevens = new int[count];
+            int index = 0;
+            for (int i = a; i <= b; i++)
+            {
+                digitCount = (int)Math.Log10(i);
+                int r = i / (int)Math.Pow(10, digitCount);
+                if (r == 7)
+                {
+                    sevens[count - 1] = i;
+                    count--;
+                }
+            }
+            foreach (var item in sevens)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //Console.Write("massivin uzunlugu daxil et");
+            //int n = int.Parse(Console.ReadLine());
+            //int [] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"arr[{i}]=");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+        }
+    }
+}
